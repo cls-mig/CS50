4e7777579d0cc3e63a5c96cd0d01a38f6d23a4c6
Carlos Miguel <carlosmiguel7952@gmail.com>
2024-02-07 23:36:37 -0300

    Atualização dos módulos

--- Módulo 1 - C/Lab 1/Lab 1 - População.c
+++ Módulo 1 - C/Lab 1/Lab 1 - População.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <math.h> // Fornece um conjunto de funções para operações matemáticas, tais como funções trigonométricas, hiperbólicas, logaritmos, potência e arredondamentos
 
-int main()
+// Laboratório 1: crescimento populacional
+/*
+- porque quando i é um float, o quantidade inicial sendo 9 e o quantidade final sendo 18, se passam 9 anos e não 8?
+*/
+int main(void)
 {
+    int years = 0; // quantidade anos que passam até o número inicial passar ou ser igual ao número final
     
+    // TODO: Solicite o valor inicial ao usuário
+    int i = 0;
+    do
+    {
+        printf("Quantidade inicial de lhamas: ");
+        scanf("%i", &i);
+        // scanf("%.0f", &i); // %.10f permite exibir um número determinado de casas após a vírgula
+    } while (i < 9);
+
+    // TODO: Solicite o valor final ao usuário
+    int f = 0;
+    do
+    {
+        printf("Quantidade final de lhamas: ");
+        scanf("%i", &f);
+    } while (f < i);
+
+    // TODO: Calcule o número de anos até o limite
+    while (!(i >= f))
+    {
+        i = round(i + (i / 3) - (i / 4)); // é amplamente utilizado para arredondar números em C. Ele recebe dois parâmetros: o número a ser arredondado e o número de casas decimais para arredondar (ex.: 2.56 * 100 = 256)
+        years++;
+    }
+
+    // TODO: Imprima o número de anos
+    printf("Years: %i", years);
 }
